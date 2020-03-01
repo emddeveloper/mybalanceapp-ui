7127b9ecc2a6245d52afa8f0d4c363f93aaa0054
SK AMINUL ISLAM <you@example.com>
2020-03-01 11:06:57 +0530

    full project upload

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" class="container">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view class="row" />
+    <footer>Developed and Maintained by <a href="https://in.linkedin.com/in/skaminul" target="_blank">SK Aminul Islam</a></footer>
+  </div>
+</template>
+
+<style>
+@import'~bootstrap/dist/css/bootstrap.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background:#b3e5fc;
+  height: 100vh;
+}
+footer{
+  font-size: 12px;
+}
+#nav {
+  padding-top:15px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance:textfield;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <BalanceForm mtitle="My Balance App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BalanceForm from '../components/BalanceForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    BalanceForm
+  }
+}
+</script>
